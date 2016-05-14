79613c17d4f82756b62658fc73d6fb01ff5d344e
narasaki <mduckfrogbby@gmail.com>
2016-05-14 17:45:26 +0900

    task/63035

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
+
+  get "/auth/:provider/callback" => "sessions#callback"
+  get "/signout" => "sessions#destroy"
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
