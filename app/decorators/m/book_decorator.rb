092dbc2a99d1b129a3a4d9753b653a42d38f31d9
rikukawano <Riku@Rikus-MacBook-Pro.local>
2016-08-18 19:31:25 +0900

    refs task #72410 ISBNのチェックを追加

--- app/decorators/m/book_decorator.rb
+++ app/decorators/m/book_decorator.rb
@@ -1,6 +1,6 @@
 module M::BookDecorator
   BOOK_IMAGE_URL_FMT = "http://images-jp.amazon.com/images/P/%s.09.TZZZZZZZ.jpg"
   def book_image_path
-    BOOK_IMAGE_URL_FMT % isbn10
+    BOOK_IMAGE_URL_FMT % self.isbn10
   end
 end
--- app/models/m/book.rb
+++ app/models/m/book.rb
@@ -5,7 +5,7 @@ class M::Book < ActiveRecord::Base
   has_many :purchase_requests
 
   validates :name, presence: true
-  validates :isbn, presence: true, uniqueness: true
+  validates :isbn, presence: true, numericality: true
 
   def requested_by? user
     PurchaseRequest.by_user_book(user.id, self.id).present?
@@ -13,30 +13,49 @@ class M::Book < ActiveRecord::Base
 
   class << self
     def create_new_book name, isbn
+      isbn = remove_hyphen isbn
+      if valid_length? isbn
+        convert_to_isbn13 isbn
+        find_or_create_book name, isbn
+      else
+        new name: name, isbn: isbn
+      end
+    end
+
+    private
+    def convert_to_isbn13 isbn
+      return isbn if isbn.blank? || isbn.length == 13
+      isbn_base = "978#{isbn[0..8]}"
+      check_digit = 0
+      isbn_base.split(//).each_with_index do |chr, idx|
+        check_digit += chr.to_i * (idx.even? ? 1 : 3)
+      end
+      check_digit = (10 - check_digit % 10) % 10
+      "#{isbn_base}#{check_digit}"
+    end
+
+    def remove_hyphen isbn
+      isbn.gsub"-", ""
+    end
+
+    def valid_length? isbn
+      isbn.length == 13 || isbn.length == 10
+    end
+
+    def find_or_create_book name, isbn
       find_by(isbn: isbn) || create(name: name, isbn: isbn)
     end
   end
 
-  def isbn13
-    return self.isbn if self.isbn.blank? || self.isbn.length == 13
-    isbn_base = "978#{self.isbn[0..8]}"
-    check_digit = 0
-    isbn_base.split(//).each_with_index do |chr, idx|
-      check_digit += chr.to_i * (idx.even? ? 1 : 3)
-    end
-    check_digit = (10 - (check_digit % 10)) % 10
-    "#{isbn_base}#{check_digit}"
-  end
-
   def isbn10
     return self.isbn if self.isbn.blank? || self.isbn.length == 10
-    isbn_base = self.isbn[3..11]
+    isbn_base = self.isbn.gsub("-", "")[3..11]
     return self.isbn if isbn_base.blank?
     check_digit = 0
     isbn_base.split(//).each_with_index do |chr, idx|
       check_digit += chr.to_i * (10 - idx)
     end
-    check_digit = 11 - (check_digit % 11)
+    check_digit = 11 - check_digit % 11
     case check_digit
     when 10
       check_digit = "X"
